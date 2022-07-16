a062f7a3a6dd3f1c575fab42d5a504a0640df813
RanjithKumarArichandran <ranjithkumarkarthic@gmail.com>
2022-07-16 07:39:49 -0700

    stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -1,7 +1,6 @@
 import os 
 import yaml
-import pandas
-import argparse
+import pandas as pd
 
 def read_params(config_path):
     with open(config_path) as yaml_file:
@@ -14,9 +13,9 @@ def get_data(config_path):
     data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=",", encoding="utf-8")
     print(df.head())
-    
+
 if __name__ == "__maim__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    get_data(config_path=parsed_args.config)
+    data = get_data(config_path=parsed_args.config)
